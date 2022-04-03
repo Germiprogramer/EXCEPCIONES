4e866c5809849cc5fab1662d99948d03741f7b5a
Germiprogramer <llorentemunozgerman@gmail.com>
2022-04-03 17:09:46 +0200

    reaslizando la funcion comprobacion

--- comprobacion.py
+++ comprobacion.py
@@ -1,4 +1,17 @@
 from correo import *
 import re
 
-def comprobacion():
+correo = Correo()
+
+def comprobacion(correo):
+    buscararroba = re.search("@",correo.get_direccion())
+    if buscararroba == None:
+        print("Una dirección de correo debe contener un @, correo invalidado")
+        print("reinicie el programa para introducir de nuevo el correo")
+    else:
+        pass
+    es_seguracom = re.search(".com",correo.get_direccion())
+    es_seguraes = re.search(".es",correo.get_direccion())
+    if es_seguracom == None and es_seguraes == None:
+        print("Cuenta bloqueada a causa de ataque")
+
--- correo.py
+++ correo.py
@@ -4,3 +4,5 @@ class Correo:
     def __init__(self, usuario, direccion):
         self.usuario = input("Identifiquese :")
         self.direccion = input("Escriba el correo :")
+    def get_direccion(self):
+        return self.direccion
