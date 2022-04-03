1d55c281c1722d8cc6465754b3178d00a873b7bd
Germiprogramer <llorentemunozgerman@gmail.com>
2022-04-03 17:20:39 +0200

    corregiendo errores en el codigo

--- correo.py
+++ correo.py
@@ -2,7 +2,9 @@ import re
 
 class Correo:
     def __init__(self, usuario, direccion):
-        self.usuario = input("Identifiquese :")
-        self.direccion = input("Escriba el correo :")
+        self.usuario = usuario
+        self.direccion = direccion
+    def get_usuario(self):
+        return self.usuario
     def get_direccion(self):
         return self.direccion
